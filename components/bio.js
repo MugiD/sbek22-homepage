b91c463ff02f16609ae58c41ed92fa4fd24dab06
Бек Сламбек <slambek_b0607@gmail.com>
2022-07-28 21:48:12 +0600

    add bio section

---
+++ components/bio.js
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+export const BioSection = styled(Box)`
+  padding-left: 3.4em;
+  text-indent: -3.4em;
+  margin-left: 20px;
+  margin-right: 20px;
+`
+
+export const BioYear = styled.span`
+  font-weight: bold;
+  margin-right: 1em;
+  margin-left: 20px;
+`
--- pages/index.js
+++ pages/index.js
@@ -14,40 +14,39 @@ import Image from "next/image";
 import Section from "../components/section";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import Paragraph from "../components/paragraph";
-import { IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
+import { BioSection, BioYear } from "../components/bio";
 
 const ProfileImage = chakra(Image, {
+  shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
+});
+const BackImage = chakra(Image, {
   shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
 });
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
-        css={{ backdropFilter: "blur(10px)" }}
-      >
-        Inspired by{" "}
-        <Link
-          href="https://youtu.be/bSMZgXzC9AA"
-          color={useColorModeValue('purple', 'orange')}
-          fontWeight="bold"
-        >
-          this
-        </Link>{" "}
-        tutorial
-      </Box>
+    <Container
+      pb={2}
+      mt={2}
+      px={0}
+      bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+      borderRadius="10px"
+    >
+      <BackImage
+        width="1000px"
+        height="400px"
+        src="/images/saitama.png"
+        alt="saitama"
+        borderRadius="10px 10px 0 0"
+      />
 
-      <Box display={{ md: "flex " }}>
+      <Box display={{ md: "flex " }} mx={5}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Bek Slambek
           </Heading>
-          <p>Saitama's idol ( Teenager / Developer / Designer )</p>
+          <p>Enthusiast, Developer, Designer</p>
         </Box>
         <Box
           flexShrink={0}
@@ -64,6 +63,9 @@ const Page = () => {
             display="inline-block"
             borderRadius="full"
             overflow="hidden"
+            position={{ md: "relative", base: "static" }}
+            top="-10"
+            right="0"
           >
             <ProfileImage
               src="/images/profileBek.png"
@@ -75,15 +77,45 @@ const Page = () => {
           </Box>
         </Box>
       </Box>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        css={{ backdropFilter: "blur(10px)" }}
+        mx={5}
+      >
+        Welcome to my homepage!
+      </Box>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        css={{ backdropFilter: "blur(10px)" }}
+        mx={5}
+      >
+        Inspired by{" "}
+        <Link
+          href="https://youtu.be/bSMZgXzC9AA"
+          color={useColorModeValue("purple", "orange")}
+          fontWeight="bold"
+        >
+          this
+        </Link>{" "}
+        tutorial
+      </Box>
 
       <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
+        <Heading as="h3" variant="section-title" mx={5}>
           About
         </Heading>
         <Paragraph>
-          Bek is a full-stack developer born in Almaty. He works with JavaScript
-          frameworks like Next.js or React. Bek believes that YTube tutorials
-          can help him to become a cool Web Developer
+          Bek is a full-stack developer from Kazakhstan. He works with different
+          technologies starting from JavaScript and ending with C#. Bek believes that YTube
+          tutorials can help him to become a PROgrammer
         </Paragraph>
         <Box align="center" my={4}>
           <NextLink href="/works" passHref scroll={false}>
@@ -95,19 +127,55 @@ const Page = () => {
       </Section>
 
       <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
+        <Heading as="h3" variant="section-title" mx={5}>
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>07/22/2006</BioYear> Born in Almaty, Kazakhstan
+        </BioSection>
+        <BioSection>
+          <BioYear>05/20/2019</BioYear> Entering in Nazarbayev Intellectual School
+        </BioSection>
+        <BioSection>
+          <BioYear>07/01/2020</BioYear> Going through courses of Front-end developer
+        </BioSection>
+        <BioSection>
+          <BioYear>07/28/2020</BioYear> Watching One Piece barely in two months
+        </BioSection>
+        <BioSection>
+          <BioYear>04/30/2022</BioYear> Partisipating in school Hackathon
+        </BioSection>
+        <BioSection>
+          <BioYear>2022 - now</BioYear> Web Development, Introducing to ML
+        </BioSection>
+        <Box
+        borderRadius="lg"
+        my={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        css={{ backdropFilter: "blur(10px)" }}
+        mx={5}
+      >
+        To be continued...
+      </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title" mx={5}>
           Things I love
         </Heading>
         <Paragraph>
-          Sport, Video Games, Anime, Food, Design, Web Development, Wasting Time
+          Sport, Video Games, Anime, Food, Design, Web Development, Machine
+          Learning, Wasting Time
         </Paragraph>
       </Section>
 
       <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
+        <Heading as="h3" variant="section-title" mx={5}>
           My Contacts
         </Heading>
-        <List>
+        <List mx={5}>
           <ListItem>
             <Link href="https://github.com/MugiD" target="_blank">
               <Button
@@ -120,13 +188,24 @@ const Page = () => {
             </Link>
           </ListItem>
           <ListItem>
-            <Link href="https://instagram.com/sbek22" target="_blank">
+            <Link href="https://twitter.com/sbek22_" target="_blank">
               <Button
                 variant="ghost"
                 colorScheme="teal"
                 leftIcon={<IoLogoInstagram />}
               >
                 @sbek22
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://linkedin.com/in/sbek22" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoLinkedin />}
+              >
+                Bek Slambek
               </Button>
             </Link>
           </ListItem>
