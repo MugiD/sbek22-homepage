fd04ac2d92e51873d37b12ba391f9f979f1db81a
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-20 22:30:32 +0600

    remove progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,6 @@ import Chakra from "../components/chakra";
 import Layout from "../components/layouts/main";
 import Fonts from "../components/fonts";
 import { AnimatePresence } from "framer-motion";
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import Router from 'next/router';
-
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
-Router.events.on('routeChangeError', () => NProgress.done());
 
 if (typeof window !== "undefined") {
   window.history.scrollRestoration = "manual";
