b42c8329340d91d77cc147f8acf49733c4f75f57
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-25 21:39:45 +0600

    add meta tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Navbar from "./Navbar.js";
-import { Box} from "@chakra-ui/react";
-import Footer from './Footer.js'
+import { Box, Container } from "@chakra-ui/react";
+import Footer from "./Footer.js";
 
 const Main = ({ children, router }) => {
   return (
@@ -9,9 +9,28 @@ const Main = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Homepage | Bek Slambek</title>
+        <link rel="shortcut icon" href="/images/chopper.jpg" type="image/x-icon" />
+        <meta
+          property="description"
+          content="Bek Slambek's homepage. Really worth your attention"
+        />
+        <meta property="og:image" content="https://sbek22.xyz/images/card.png" />
+        <meta property="og:title" content="Homepage | Bek Slambek" />
+        <meta
+          property="og:description"
+          content="Bek Slambek's homepage. Really worth your attention"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content="Homepage | Bek Slambek" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@sbek22_" />
+        <meta
+          name="twitter:image"
+          content="https://sbek22.xyz/images/card.png"
+        />
       </Head>
       <Navbar path={router.asPath} />
-      {children}
+      <Container maxW="container.lg">{children}</Container>
       <Footer />
     </Box>
   );
