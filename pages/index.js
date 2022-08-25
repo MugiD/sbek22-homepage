a5e3131a03afa72e32504a50d925885cf1e43224
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-25 21:40:14 +0600

    redesign

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import { Container } from "@chakra-ui/react";
 import Intro from "../components/MainPage/Intro";
 import About from "../components/MainPage/About";
-import Bio from "../components/MainPage/Journey";
 
 const Home = () => {
   return (
     <Container maxW="container.lg" pt={14}>
       <Intro />
       <About />
-      <Bio />
     </Container>
   );
 };
