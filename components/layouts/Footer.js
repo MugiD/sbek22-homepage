6f783e28dc7b296bff880be129f7c26e3191f8c6
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-26 09:45:41 +0600

    change icons' size

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -1,69 +1,69 @@
 import {
-  IoLogoLinkedin,
-  IoLogoTwitter,
-  IoLogoInstagram,
-  IoLogoGithub,
-} from "react-icons/io5";
-import {
-  chakra,
-  Container,
-  Stack,
-  VisuallyHidden,
-  useColorModeValue,
-  Icon
-} from "@chakra-ui/react";
-
-const SocialButton = ({ children, label, href }) => {
-  return (
-    <chakra.button
-      as="a"
-      rounded="full"
-      w={20}
-      h={20}
-      cursor="pointer"
-      href={href}
-      display="inline-flex"
-      alignItems="center"
-      justifyContent="center"
-      transition="background 0.3s ease"
-      target="_blank"
-      rel="noreferrer"
-      _hover={{ bg: "whiteAlpha.200" }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
-
-export default function Footer() {
-  return (
-    <Container
-      as={Stack}
-      maxW="container.lg"
-      my={10}
-      py={4}
-      direction={{ base: "column", md: "row" }}
-      spacing={4}
-      justify={{ base: "center", md: "space-between" }}
-      align="center"
-      borderTop="solid"
-      borderTopColor={useColorModeValue("blackAlpha.100", "whiteAlpha.200")}
-    >
-      <Stack direction="row" spacing={6}>
-        <SocialButton label="Twitter" href="https://twitter.com/sbek22_">
-          <Icon as={IoLogoTwitter} w={8} h={8} />
-        </SocialButton>
-        <SocialButton label="Instagram" href="https://instagram.com/sbek22">
-          <Icon as={IoLogoInstagram} w={8} h={8}/>
-        </SocialButton>
-        <SocialButton label="GitHub" href="https://github.com/MugiD">
-          <Icon as={IoLogoGithub} w={8} h={8}/>
-        </SocialButton>
-        <SocialButton label="LinkedIn" href="https://linkedin.com/in/sbek22">
-          <Icon as={IoLogoLinkedin}  w={8} h={8}/>
-        </SocialButton>
-      </Stack>
-    </Container>
-  );
-}
+    IoLogoLinkedin,
+    IoLogoTwitter,
+    IoLogoInstagram,
+    IoLogoGithub,
+  } from "react-icons/io5";
+  import {
+    chakra,
+    Container,
+    Stack,
+    VisuallyHidden,
+    useColorModeValue,
+    Icon
+  } from "@chakra-ui/react";
+  
+  const SocialButton = ({ children, label, href }) => {
+    return (
+      <chakra.button
+        as="a"
+        rounded="full"
+        w={16}
+        h={16}
+        cursor="pointer"
+        href={href}
+        display="inline-flex"
+        alignItems="center"
+        justifyContent="center"
+        transition="background 0.3s ease"
+        target="_blank"
+        rel="noreferrer"
+        _hover={{ bg: "whiteAlpha.200" }}
+      >
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    );
+  };
+  
+  export default function Footer() {
+    return (
+      <Container
+        as={Stack}
+        maxW="container.lg"
+        my={10}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align="center"
+        borderTop="solid"
+        borderTopColor={useColorModeValue("blackAlpha.100", "whiteAlpha.200")}
+      >
+        <Stack direction="row" spacing={6}>
+          <SocialButton label="Twitter" href="https://twitter.com/sbek22_">
+            <Icon as={IoLogoTwitter} w={8} h={8} />
+          </SocialButton>
+          <SocialButton label="Instagram" href="https://instagram.com/sbek22">
+            <Icon as={IoLogoInstagram} w={8} h={8}/>
+          </SocialButton>
+          <SocialButton label="GitHub" href="https://github.com/MugiD">
+            <Icon as={IoLogoGithub} w={8} h={8}/>
+          </SocialButton>
+          <SocialButton label="LinkedIn" href="https://linkedin.com/in/sbek22">
+            <Icon as={IoLogoLinkedin}  w={8} h={8}/>
+          </SocialButton>
+        </Stack>
+      </Container>
+    );
+  }
